d573aab7ccfa0541a1d017673b23d6dac93150af
VIVEK KUMAR SAHU <113853773+viveksahu06@users.noreply.github.com>
2023-10-03 00:09:01 +0530

    Add files via upload
    The algorithm implemented is the Merge Sort algorithm applied to a singly linked list. Merge Sort is a popular and efficient comparison-based sorting algorithm known for its stable and consistent performance in various scenarios, including sorting linked lists.
    
    Here's a description of how the Merge Sort algorithm works for a linked list:
    
    Divide: The input linked list is divided into two roughly equal halves. To do this, you find the middle point of the list. This is typically done using two pointers: a slow pointer that moves one step at a time and a fast pointer that moves two steps at a time. When the fast pointer reaches the end of the list, the slow pointer will be at the middle.
    
    Recursively Sort: Once you've divided the list into two halves, you recursively apply the Merge Sort algorithm to each half. This process continues until you have divided the list into smaller sublists of length 1 or 0, which are, by definition, sorted.
    
    Merge: The heart of the Merge Sort algorithm is the merging step. You take the two sorted sublists resulting from the previous step and merge them into a single sorted list. This is done by comparing the elements from the two sublists and selecting the smaller of the two at each step to build the merged list.
    
    Combine: Continue merging the sublists until you have a single sorted list that contains all the elements from the original list.
    
    Result: The final result is a sorted linked list containing all the elements from the input list.
    
    The key advantages of Merge Sort are its stable O(n log n) time complexity for sorting, which is very efficient, and its ability to be easily adapted for sorting linked lists. Merge Sort guarantees stable sorting, which means that equal elements retain their relative order in the sorted output, making it suitable for various applications.

---
+++ LinkedListMergeSort.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+public class LinkedListMergeSort { 
+   public static class Node {
+        int data; 
+        Node next;  
+
+        public Node(int data) {
+            this.data = data; 
+            this.next = null;
+        }   
+    }
+
+        public static Node head;  
+        public static Node tail;
+
+        public void addFirst(int data) { 
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            } 
+            newNode.next = head; 
+            head = newNode;
+        } 
+
+        public void printList() { 
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->"); 
+                temp = temp.next;
+            } 
+            System.out.println();
+        } 
+
+
+        public Node mergeSort(Node head){
+             if(head==null || head.next==null) {
+                return head;
+             } 
+             //find mid 
+             Node mid=getMid(head); 
+             Node righthead=mid.next; 
+              mid.next=null; 
+
+             Node newLeft=mergeSort(head); 
+             Node newRight=mergeSort(righthead);
+             return merge(newLeft,newRight);
+        } 
+
+        private Node getMid(Node head){
+            Node slow = head;
+            Node fast = head.next;
+        
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+        
+            return slow;
+        } 
+
+        private Node merge(Node head1,Node head2){
+            Node mergedLL=new Node(-1); 
+            Node temp=mergedLL; 
+
+            while(head1!=null && head2!=null){
+                if(head1.data<=head2.data){
+                    temp.next=head1; 
+                    head1=head1.next;
+                    temp=temp.next;  
+                } 
+                else{
+                    temp.next=head2; 
+                    head2=head2.next;
+                    temp=temp.next; 
+                } 
+               
+            } 
+            
+            //left sub 
+            while(head1!=null){
+                temp.next=head1; 
+                head1=head1.next; 
+                temp=temp.next;
+            }
+            //right sub
+            while(head2!=null){
+                temp.next=head2; 
+                head2=head2.next; 
+                temp=temp.next;
+            } 
+
+            return mergedLL.next;
+        }
+    
+
+    public static void main(String[] args) {
+        LinkedListMergeSort ll = new LinkedListMergeSort(); 
+       ll.addFirst(1); 
+       ll.addFirst(2);
+       ll.addFirst(3);
+       ll.addFirst(4);
+       ll.addFirst(5);
+       ll.printList();  
+       ll.head=ll.mergeSort(ll.head); 
+       ll.printList();
+
+
+    }
+}
